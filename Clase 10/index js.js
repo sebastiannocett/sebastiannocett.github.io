754c7a3de1a36d34917d062c167402745a9e5e5b
sebastiannocetti <130950369+sebastiannocett@users.noreply.github.com>
2023-05-21 22:50:09 -0300

    Clase 10

---
+++ Clase 10/index js.js
@@ -0,0 +1,12 @@
+//variables para nombre
+const nombre = "sebastian"; //string
+const apellido = "nocetti"; //string
+console.log(nombre + apellido) 
+
+let numero = 150; 
+let numero2 = 30;
+let resultado = numero + numero2
+console.log(resultado)
+let numero3 = 20
+let resultado2 = resultado + numero3
+console.log(resultado2)
